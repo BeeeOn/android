f838e7460cfb1e8c0f5ca3fa32ca4f23220dd01e
Robert Pösel <xposel00@stud.fit.vutbr.cz>
2014-08-09 23:41:37 +0200

    Add more adapters for Demo Mode

--- IHA/src/cz/vutbr/fit/iha/household/DemoHousehold.java
+++ IHA/src/cz/vutbr/fit/iha/household/DemoHousehold.java
@@ -43,9 +43,15 @@ public final class DemoHousehold extends Household {
 		
 		try {
 			Adapter adapter = XmlParsers.getDemoAdapterFromAsset(mContext, Constants.ASSET_ADAPTERS_FILENAME);
-			List<Location> locations = XmlParsers.getDemoLocationsFromAsset(mContext, Constants.ASSET_LOCATIONS_FILENAME);
-			adapter.setLocations(locations);
-			
+			adapter.setId("01233145645792"); // original from xml
+			adapter.setName("Home adapter");
+			adapter.setLocations(XmlParsers.getDemoLocationsFromAsset(mContext, Constants.ASSET_LOCATIONS_FILENAME));
+			this.adapters.add(adapter);
+
+			adapter = XmlParsers.getDemoAdapterFromAsset(mContext, Constants.ASSET_ADAPTERS_FILENAME);
+			adapter.setId("16457841561538"); // random one
+			adapter.setName("Testing duplicate");
+			adapter.setLocations(XmlParsers.getDemoLocationsFromAsset(mContext, Constants.ASSET_LOCATIONS_FILENAME));
 			this.adapters.add(adapter);
 		}
 		catch(Exception e){
