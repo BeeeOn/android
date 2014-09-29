019c3d4e78dd0c40b52f73608aa18c2cf1b469db
Robert Pösel <xposel00@stud.fit.vutbr.cz>
2014-09-29 10:37:13 +0200

    Add more widget sizes (1x1, 2x1, 3x1) (for old Androids)

---
+++ IHA/src/cz/vutbr/fit/iha/widget/SensorWidgetProviderSmall.java
@@ -0,0 +1,5 @@
+package cz.vutbr.fit.iha.widget;
+
+public final class SensorWidgetProviderSmall extends SensorWidgetProvider {
+	// Leave this empty as we're working in WidgetUpdateService with SensorWidgetProvider directly
+}
