d0dc112c9d21ba1213042aa3578ebbf620dbe6f5
Martin Douděra <xdoude00@stud.fit.vutbr.cz>
2014-10-12 21:21:36 +0200

    Added back button, changed logo in AB

--- IHA/src/cz/vutbr/fit/iha/activity/MapGeofenceActivity.java
+++ IHA/src/cz/vutbr/fit/iha/activity/MapGeofenceActivity.java
@@ -3,6 +3,7 @@ package cz.vutbr.fit.iha.activity;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
@@ -16,6 +17,13 @@ public class MapGeofenceActivity extends SherlockFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map_geofence);
+		
+		setSupportProgressBarIndeterminate(true);
+		setProgressBarIndeterminateVisibility(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setIcon(R.drawable.ic_launcher_white);
+		
 		mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 
 		if (mMap != null) {
@@ -24,5 +32,15 @@ public class MapGeofenceActivity extends SherlockFragmentActivity {
 			mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 			mMap.setMyLocationEnabled(true);
 		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return true;
+		}
+		return false;
 	}
 }
