f62b41d46b382325c623028135282d064296014f
Martin Douděra <xdoude00@stud.fit.vutbr.cz>
2015-02-20 19:08:22 +0100

    Asynchronous loading map, draggable marker example

--- app/src/main/java/com/rehivetech/beeeon/activity/MapGeofenceActivity.java
+++ app/src/main/java/com/rehivetech/beeeon/activity/MapGeofenceActivity.java
@@ -2,7 +2,6 @@ package com.rehivetech.beeeon.activity;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
 
 import android.content.Context;
 import android.graphics.Color;
@@ -13,8 +12,6 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
@@ -23,19 +20,19 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuItem.OnActionExpandListener;
-import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.rehivetech.beeeon.R;
@@ -43,13 +40,14 @@ import com.rehivetech.beeeon.base.BaseActivity;
 import com.rehivetech.beeeon.geofence.SimpleGeofence;
 import com.rehivetech.beeeon.util.Log;
 
-public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickListener {
+public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickListener, OnMarkerDragListener,
+		OnMapReadyCallback {
 
 	private static final String TAG = "geofence";
 
 	private static final float MAP_ZOOM = 17.5F;
 	private static final int MAXIMUM_RET_ADDRESS = 3;
-	
+
 	private EditText mEditSearch;
 
 	private GoogleMap mMap;
@@ -65,40 +63,8 @@ public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickL
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setIcon(R.drawable.ic_launcher_null);
 
-		mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
 
-		if (mMap != null) {
-			// it needs permission in manifest
-			mMap.setMyLocationEnabled(true);
-
-			mMap.setOnMapLongClickListener(this);
-
-			// Sets the map type to be "hybrid"
-			mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-			// trying to zoom to actual position
-			LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-			Criteria criteria = new Criteria();
-			String provider = locationManager.getBestProvider(criteria, true);
-			Location location = locationManager.getLastKnownLocation(provider);
-			if (location != null) {
-
-				LatLng actlatLng = new LatLng(location.getLatitude(), location.getLongitude());
-				mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(actlatLng, MAP_ZOOM));
-
-				// CameraPosition INIT = new CameraPosition.Builder()
-				// .target(actlatLng)
-				// .zoom(MAP_ZOOM)
-				// .bearing(300F) // orientation
-				// .tilt(50F) // viewing angle
-				// .build();
-				//
-				// mMap.animateCamera(CameraUpdateFactory.newCameraPosition(INIT));
-			} else {
-				Log.d(TAG, "Location is not accesible.");
-			}
-
-		}
 	}
 
 	// Create the options menu
@@ -123,7 +89,7 @@ public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickL
 						Log.d(TAG, "Not null");
 						new GeocoderTask().execute(searchString);
 					}
-					
+
 					// hide keyboard
 					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 					imm.hideSoftInputFromWindow(mEditSearch.getWindowToken(), 0);
@@ -175,8 +141,40 @@ public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickL
 	}
 
 	@Override
-	public void onMapLongClick(LatLng arg0) {
-		Toast.makeText(this, "Long Click", Toast.LENGTH_LONG).show();
+	public void onMapReady(GoogleMap map) {
+		// it needs permission in manifest
+		this.mMap = map;
+		mMap.setMyLocationEnabled(true);
+
+		mMap.setOnMapLongClickListener(this);
+
+		// Sets the map type to be "hybrid"
+		mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		mMap.setOnMarkerDragListener(this);
+
+		// trying to zoom to actual position
+		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		String provider = locationManager.getBestProvider(criteria, true);
+		Location location = locationManager.getLastKnownLocation(provider);
+		if (location != null) {
+
+			LatLng actlatLng = new LatLng(location.getLatitude(), location.getLongitude());
+			mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(actlatLng, MAP_ZOOM));
+
+			// CameraPosition INIT = new CameraPosition.Builder()
+			// .target(actlatLng)
+			// .zoom(MAP_ZOOM)
+			// .bearing(300F) // orientation
+			// .tilt(50F) // viewing angle
+			// .build();
+			//
+			// mMap.animateCamera(CameraUpdateFactory.newCameraPosition(INIT));
+		} else {
+			Log.d(TAG, "Location is not accesible.");
+		}
+		
+		map.addMarker(new MarkerOptions().draggable(true).position(new LatLng(10, 10)).title("Hello world"));
 	}
 
 	public void addMarkerForFence(SimpleGeofence fence) {
@@ -205,9 +203,9 @@ public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickL
 			// Creating an instance of Geocoder class
 			Geocoder geocoder = new Geocoder(getBaseContext());
 			List<Address> addresses = null;
-			
+
 			Log.d(TAG, "Start searching:" + locationName);
-			
+
 			try {
 				// Getting a maximum of 3 Address that matches the input text
 				addresses = geocoder.getFromLocationName(locationName[0], MAXIMUM_RET_ADDRESS);
@@ -225,7 +223,7 @@ public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickL
 			}
 
 			// Clears all the existing markers on the map
-			//mMap.clear();
+			// mMap.clear();
 
 			// Adding Markers on Google Map for each matching address
 			for (int i = 0; i < addresses.size(); i++) {
@@ -250,5 +248,30 @@ public class MapGeofenceActivity extends BaseActivity implements OnMapLongClickL
 					mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
 			}
 		}
+	}
+
+	/** CALLBACKS **/
+
+	@Override
+	public void onMapLongClick(LatLng arg0) {
+		Toast.makeText(this, "Long Click", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onMarkerDrag(Marker arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onMarkerDragEnd(Marker arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onMarkerDragStart(Marker arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
