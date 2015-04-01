37ef415d6d69d050b30e64b44de068250a1ce15e
Martin Douděra <xdoude00@stud.fit.vutbr.cz>
2015-04-01 19:07:27 +0200

    Added login control when async re-registration occur

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gcm/GcmReRegistrationHandler.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gcm/GcmReRegistrationHandler.java
@@ -3,6 +3,7 @@ package com.rehivetech.beeeon.gcm;
 import android.app.IntentService;
 import android.content.Intent;
 
+import com.rehivetech.beeeon.controller.Controller;
 import com.rehivetech.beeeon.util.Log;
 
 public class GcmReRegistrationHandler extends IntentService {
@@ -19,8 +20,12 @@ public class GcmReRegistrationHandler extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		Log.i(TAG, GcmHelper.TAG_GCM + "Re-registartion GCM");
-		GcmHelper.registerGCMInBackground(getApplicationContext());
+		if (Controller.getInstance(this).isLoggedIn()) {
+			Log.i(TAG, GcmHelper.TAG_GCM + "Re-registartion GCM");
+			GcmHelper.registerGCMInBackground(getApplicationContext());
+		} else {
+			GcmHelper.invalidateLocalGcmId(Controller.getInstance(this));
+		}
 	}
 
 }
