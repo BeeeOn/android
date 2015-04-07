7386fa2bed3d9f146a4b62d4efa1a630dcadcee3
Leopold Podmolík <xpodmo01@stud.fit.vutbr.cz>
2015-04-08 00:41:33 +0200

    func com

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/network/Network.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/network/Network.java
@@ -107,7 +107,7 @@ public class Network implements INetwork {
 	private SSLSocket permaSocket = null;
 	private PrintWriter permaWriter = null;
 	private BufferedReader permaReader = null;
-	private static final String EOF = "</com>\n"; //FIXME: temporary solution (new version)
+	private static final String EOF = "</com>"; //FIXME: temporary solution (new version)
 	private static final String SERVER_EOF = "<com></com>";
 	private boolean mIsMulti = false;
 
@@ -216,8 +216,6 @@ public class Network implements INetwork {
 				throw AppException.wrap(e, NetworkError.SOCKET_PROBLEM);
 			}
 		}
-		
-		request += SERVER_EOF ;
 		// Send request (and close writer if not multi session)
 		sendRequest(w, request, !mIsMulti);
 
@@ -421,6 +419,8 @@ public class Network implements INetwork {
 		// Debug.startMethodTracing("Support_231");
 		// long ltime = new Date().getTime();
 		try {
+			if(!messageToSend.contains("</com>"))
+				messageToSend += SERVER_EOF;
 			Log.d(TAG + " fromApp >>", messageToSend);
 			String result = startCommunication(messageToSend);
 			Log.i(TAG + " << fromSrv", result);
