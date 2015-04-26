6f2e0dbc222837107d6623074fc7c9f1c7f5d4f5
Robyer <robyer@seznam.cz>
2015-04-26 19:30:45 +0200

    Fix GetDeviceLogTask to extends CallbackTask (fixes #553)
    All tasks must extends CallbackTask because it catches AppExceptions inside. That's why it caused crashes. This commit reworks the task to common format.

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/asynctask/GetDeviceLogTask.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/asynctask/GetDeviceLogTask.java
@@ -1,42 +1,23 @@
 package com.rehivetech.beeeon.asynctask;
 
 import android.content.Context;
-import android.os.AsyncTask;
 
-import com.rehivetech.beeeon.household.device.DeviceLog;
 import com.rehivetech.beeeon.controller.Controller;
+import com.rehivetech.beeeon.household.device.DeviceLog;
 import com.rehivetech.beeeon.pair.LogDataPair;
 
-/**
- * ASYNC TASK - for load data for graph
- */
-public class GetDeviceLogTask extends AsyncTask<LogDataPair, Void, DeviceLog> {
-	private Context mContext;
-	private CallbackLogTaskListener mListener;
+public class GetDeviceLogTask extends CallbackTask<LogDataPair> {
 
-	public GetDeviceLogTask(Context context){
-		mContext = context;
+	public GetDeviceLogTask(Context context) {
+		super(context);
 	}
 
 	@Override
-	protected DeviceLog doInBackground(LogDataPair... pairs) {
-		LogDataPair pair = pairs[0]; // expects only one device at a time is sent there
+	protected Boolean doInBackground(LogDataPair pair) {
 		Controller controller = Controller.getInstance(mContext);
+
 		// Load log data if needed
-		controller.getDeviceLogsModel().reloadDeviceLog(pair);
-		return controller.getDeviceLogsModel().getDeviceLog(pair);
+		return controller.getDeviceLogsModel().reloadDeviceLog(pair);
 	}
 
-	@Override
-	protected void onPostExecute(DeviceLog res) {
-		mListener.onExecute(res);
-	}
-
-	public final void setListener(CallbackLogTaskListener listener) {
-		mListener = listener;
-	}
-
-	public interface CallbackLogTaskListener {
-		public void onExecute(DeviceLog result);
-	}
 }
