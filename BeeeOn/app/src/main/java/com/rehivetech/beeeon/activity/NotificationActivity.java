a6a05f5fbc4f9895b4806c91a9e8449e5a196e9b
Leopold Podmolík <xpodmo01@stud.fit.vutbr.cz>
2015-05-01 17:22:06 +0200

    Notification Icon to MainActivity

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/activity/NotificationActivity.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/activity/NotificationActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,10 +17,21 @@ import com.rehivetech.beeeon.activity.fragment.NotificationFragment;
 
 public class NotificationActivity extends ActionBarActivity {
 
+	private Toolbar mToolbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_notification);
+
+		mToolbar = (Toolbar) findViewById(R.id.toolbar);
+		if (mToolbar != null) {
+			mToolbar.setTitle(R.string.title_activity_notification);
+			setSupportActionBar(mToolbar);
+		}
+
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		Fragment fragment = new NotificationFragment();
 		fragment.setArguments(getIntent().getExtras());
@@ -28,6 +40,16 @@ public class NotificationActivity extends ActionBarActivity {
 					.add(R.id.container, fragment)
 					.commit();
 		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				finish();
+				return true;
+		}
+		return false;
 	}
 
 }
