fdfeaafe797c6e0bfbaeede0968ee835e708abf8
Robyer <robyer@seznam.cz>
2015-05-28 13:45:30 +0200

    Print log of devices

--- DevicesTransformer/src/Main.java
+++ DevicesTransformer/src/Main.java
@@ -12,73 +12,75 @@ import java.io.PrintWriter;
 
 public class Main {
 
-    public static void main(String[] args) {
-        processDevices();
-        processLanguages();
-    }
+	public static void main(String[] args) {
+		processDevices();
+		//processLanguages();
+	}
 
 
-    private static void processDevices() {
-        File file = new File("xml/devices.xml");
+	private static void processDevices() {
+		File file = new File("xml/devices.xml");
 
-        try {
-            System.out.println(String.format("Loading devices specification from '%s'", file.getAbsolutePath()));
+		try {
+			System.out.println(String.format("Loading devices specification from '%s'", file.getAbsolutePath()));
 
-            Devices devices = DevicesParser.parse(file);
+			Devices devices = DevicesParser.parse(file);
 
-            File dir = new File("export/objects/");
-            dir.mkdirs();
+			devices.printLog(System.out);
 
-            String name = "devices.java";
-            File output = new File(dir, name);
+			File dir = new File("export/objects/");
+			dir.mkdirs();
 
-            System.out.println(String.format("Saving devices objects to '%s'", output.getAbsolutePath()));
-            PrintWriter writer = new PrintWriter(output, "UTF-8");
+			String name = "devices.java";
+			File output = new File(dir, name);
 
-            devices.printDevicesJava(writer);
+			System.out.println(String.format("Saving devices objects to '%s'", output.getAbsolutePath()));
+			PrintWriter writer = new PrintWriter(output, "UTF-8");
 
-            writer.close();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        }
-    }
+			devices.printDevicesJava(writer);
 
-    private static void processLanguages() {
-        File[] files = new File("xml/languages/").listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.startsWith("language_") && name.endsWith(".xml");
-            }
-        });
+			writer.close();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+	}
 
-        for (File file : files) {
-            try {
-                System.out.println(String.format("Loading translation from '%s'", file.getAbsolutePath()));
+	private static void processLanguages() {
+		File[] files = new File("xml/languages/").listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.startsWith("language_") && name.endsWith(".xml");
+			}
+		});
 
-                Language language = LanguageParser.parse(file);
+		for (File file : files) {
+			try {
+				System.out.println(String.format("Loading translation from '%s'", file.getAbsolutePath()));
 
-                File dir = new File(String.format("export/values-%s/", language.getCode()));
-                dir.mkdirs();
+				Language language = LanguageParser.parse(file);
 
-                String name = "generated_strings_devices.xml";
-                File output = new File(dir, name);
+				File dir = new File(String.format("export/values-%s/", language.getCode()));
+				dir.mkdirs();
 
-                System.out.println(String.format("Saving Android's strings XML to '%s'", output.getAbsolutePath()));
-                PrintWriter writer = new PrintWriter(output, "UTF-8");
-                language.printAndroidXml(writer);
-                writer.close();
-            } catch (ParserConfigurationException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (SAXException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+				String name = "generated_strings_devices.xml";
+				File output = new File(dir, name);
+
+				System.out.println(String.format("Saving Android's strings XML to '%s'", output.getAbsolutePath()));
+				PrintWriter writer = new PrintWriter(output, "UTF-8");
+				language.printAndroidXml(writer);
+				writer.close();
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (SAXException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
