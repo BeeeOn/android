06ed94ea205d547d0e05dbd58c08172b1aa442a2
xkozak15 <dkozak94@gmail.com>
2015-06-05 15:43:20 +0200

    When scanning QR code, use specific exception, fix crash when Google Play is not installed

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/fragment/AddGateFragment.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/fragment/AddGateFragment.java
@@ -1,5 +1,6 @@
 package com.rehivetech.beeeon.gui.fragment;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -12,6 +13,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.rehivetech.beeeon.R;
 import com.rehivetech.beeeon.controller.Controller;
@@ -22,6 +24,7 @@ import com.rehivetech.beeeon.util.Log;
 public class AddGateFragment extends TrackFragment {
 
 	private static final String TAG = AddGateFragment.class.getSimpleName();
+	private static final int SCAN_REQUEST = 0;
 
 	public AddGateActivity mActivity;
 	private LinearLayout mLayout;
@@ -71,11 +74,15 @@ public class AddGateFragment extends TrackFragment {
 				try {
 					Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 					intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // PRODUCT_MODE for bar codes
-					startActivityForResult(intent, 0);
-				} catch (Exception e) {
-					Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
-					Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
-					startActivity(marketIntent);
+					startActivityForResult(intent, SCAN_REQUEST);
+				} catch (ActivityNotFoundException e) {
+					try {
+						Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+						Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+						startActivity(marketIntent);
+					} catch (ActivityNotFoundException e1) {
+						Toast.makeText(getActivity(),R.string.error_no_qr_reader, Toast.LENGTH_LONG).show();
+					}
 				}
 			}
 		});
@@ -89,7 +96,7 @@ public class AddGateFragment extends TrackFragment {
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == 0 && resultCode == MainActivity.RESULT_OK) {
+		if (requestCode == SCAN_REQUEST && resultCode == MainActivity.RESULT_OK) {
 			// Fill scanned code into edit text
 			EditText serialNumberEdit = (EditText) mView.findViewById(R.id.addgate_ser_num);
 			serialNumberEdit.setText(data.getStringExtra("SCAN_RESULT"));
