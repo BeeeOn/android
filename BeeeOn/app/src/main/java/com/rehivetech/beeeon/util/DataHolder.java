87d2085c6cf6524e59c8245304f73f259d9413d5
Robyer <robyer@seznam.cz>
2015-06-06 06:48:33 +0200

    Add new SimpleDataHolder from which DataHolder extends
    This also fixes behavior of setObjects() method to clear the lastUpdate timestamp - by calling the clear() method of the object, instead of the map.

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/util/DataHolder.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/util/DataHolder.java
@@ -9,106 +9,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class DataHolder<O extends IIdentifier> {
+/**
+ * Extends SimpleDataHolder with ability to hold last update time of the data.
+ */
+public class DataHolder<O extends IIdentifier> extends SimpleDataHolder<O> {
 
-	private final Map<String, O> mObjects = new HashMap<>();
-
-	private DateTime mLastUpdate;
+	protected DateTime mLastUpdate;
 
 	/**
 	 * Clear internal objects and set last update to null.
 	 */
 	public void clear() {
-		mObjects.clear();
+		super.clear();
 		mLastUpdate = null;
-	}
-
-	/**
-	 * Return list with all objects.
-	 *
-	 * @return new List (ArrayList used here) with all objects added.
-	 */
-	public List<O> getObjects() {
-		List<O> objects = new ArrayList<>();
-
-		for (O obj : mObjects.values()) {
-			objects.add(obj);
-		}
-
-		return objects;
-	}
-
-	/**
-	 * Clear internal objects map and put all objects inside (identified by id).
-	 *
-	 * @param objects
-	 */
-	public void setObjects(List<O> objects) {
-		mObjects.clear();
-
-		for (O obj : objects) {
-			mObjects.put(obj.getId(), obj);
-		}
-	}
-
-	/**
-	 * Return object with specified id.
-	 *
-	 * @param id
-	 * @return object with specified id or {@code null}, if no object was found.
-	 */
-	public O getObject(String id) {
-		return mObjects.get(id);
-	}
-
-	/**
-	 * Return object with specified id or first object, if specified one doesn't exists.
-	 *
-	 * @param id
-	 * @return object with specified id, or first object, or {@code null}, if no object was found.
-	 */
-	public O getObjectOrFirst(String id) {
-		// Try to return existing object
-		if (mObjects.containsKey(id)) {
-			return mObjects.get(id);
-		}
-
-		// Object with specified id wasn't found, let's return first one (if any exists)
-		for (O obj : mObjects.values()) {
-			return obj;
-		}
-
-		return null;
-	}
-
-	/**
-	 * Add object to the map (identified by id).
-	 *
-	 * @param obj
-	 * @return previous object in map with this id, or {@code null} if no object existed.
-	 */
-	public O addObject(O obj) {
-		return mObjects.put(obj.getId(), obj);
-	}
-
-	/**
-	 * Remove object from the map.
-	 *
-	 * @param id
-	 * @return previous object in map with this id, or {@code null} if no object existed.
-	 */
-	public O removeObject(String id) {
-		return mObjects.remove(id);
-	}
-
-	/**
-	 * Checks if map is object with specified id.
-	 *
-	 * @param id
-	 * @return true if object with this id in map exists, or false otherwise.
-	 */
-	public boolean hasObject(String id) {
-		return mObjects.containsKey(id);
 	}
 
 	/**
