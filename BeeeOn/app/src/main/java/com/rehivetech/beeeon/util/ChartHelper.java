a888804b1d10a64c1b6dfce557982fa0efe285b8
Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
2015-07-08 14:49:13 +0200

    Remove unnecessary methods and add documentation coments.

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/util/ChartHelper.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/util/ChartHelper.java
@@ -10,16 +10,10 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.utils.ValueFormatter;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.rehivetech.beeeon.R;
 import com.rehivetech.beeeon.controller.Controller;
-import com.rehivetech.beeeon.household.device.Module;
 import com.rehivetech.beeeon.household.device.values.BaseEnumValue;
 import com.rehivetech.beeeon.household.device.values.BaseValue;
-
-import org.joda.time.format.DateTimeFormatter;
 
 import java.util.List;
 
@@ -31,94 +25,23 @@ final public class ChartHelper {
 	private ChartHelper() {
 	}
 
-
-	public static void prepareGraphView(final GraphView graphView, final Context context, final Module module, final DateTimeFormatter fmt, final UnitsHelper unitsHelper) {
-		boolean isEnumValue = module.getValue() instanceof BaseEnumValue;
-
-		graphView.setTitleTextSize(context.getResources().getDimension(R.dimen.textsizesmaller));
-		graphView.getGridLabelRenderer().setTextSize(context.getResources().getDimension(R.dimen.textsizesmaller));
-		graphView.getGridLabelRenderer().setVerticalLabelsColor(context.getResources().getColor(R.color.beeeon_text_hint));
-		graphView.getGridLabelRenderer().setHorizontalLabelsColor(context.getResources().getColor(R.color.beeeon_text_hint));
-		graphView.getGridLabelRenderer().setGridColor(context.getResources().getColor(R.color.beeeon_text_hint));
-		graphView.getPointer().setColor(context.getResources().getColor(R.color.beeeon_secundary_pink));
-		graphView.getPointer().setTextColor(context.getResources().getColor(R.color.white));
-
-		graphView.setLoading(true);
-		graphView.setDrawPointer(true);
-
-		graphView.getViewport().setScalable(true);
-		graphView.getViewport().setScrollable(true);
-		graphView.setVisibility(View.VISIBLE);
-
-		if (isEnumValue) {
-			graphView.getViewport().setYAxisBoundsManual(true);
-			graphView.getViewport().setMaxY(1.1d);
-			BaseEnumValue value = (BaseEnumValue) module.getValue();
-			List<BaseEnumValue.Item> enumItems = value.getEnumItems();
-			String[] verlabels = new String[enumItems.size()];
-			int i = 0;
-			for (BaseEnumValue.Item item : enumItems) {
-				verlabels[i++] = context.getString(item.getStringResource());
-			}
-			DateAsXAxisLabelFormatter labelFormatter = new DateAsXAxisLabelFormatter(context, "HH:mm", "dd.MM.yy");
-			StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView, labelFormatter);
-			staticLabelsFormatter.setVerticalLabels(verlabels);
-			graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-		} else {
-			final String unit = " " + unitsHelper.getStringUnit(module.getValue());
-			graphView.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(context, "HH:mm", "dd.MM.yy", unit));
-		}
-
-	}
-
-
 	/**
-	 * Preparation of skin for widget graph
-	 *
-	 * @param graphView
-	 * @param context
+	 * Set chart params, legend and value formatter
+	 * @param chart chart instance
+	 * @param context context
 	 * @param baseValue
-	 * @param fmt
-	 * @param unitsHelper
+	 * @param layout layout for adding textviews
+	 * @param controller Controller instance
 	 */
-	public static void prepareWidgetGraphView(final GraphView graphView, final Context context, final BaseValue baseValue, final DateTimeFormatter fmt, final UnitsHelper unitsHelper) {
-		float textSize = context.getResources().getDimension(R.dimen.textsize_caption);
-
-		graphView.getGridLabelRenderer().setTextSize(textSize);
-		graphView.getGridLabelRenderer().setVerticalLabelsColor(context.getResources().getColor(R.color.beeeon_text_hint));
-		graphView.getGridLabelRenderer().setHorizontalLabelsColor(context.getResources().getColor(R.color.beeeon_text_hint));
-		graphView.getGridLabelRenderer().setGridColor(context.getResources().getColor(R.color.beeeon_text_hint));
-
-		boolean isEnumValue = baseValue instanceof BaseEnumValue;
-		if (isEnumValue) {
-			graphView.getViewport().setYAxisBoundsManual(true);
-			graphView.getViewport().setMaxY(1.1d);
-			BaseEnumValue value = (BaseEnumValue) baseValue;
-			List<BaseEnumValue.Item> enumItems = value.getEnumItems();
-			String[] verlabels = new String[enumItems.size()];
-			int i = 0;
-			for (BaseEnumValue.Item item : enumItems) {
-				verlabels[i++] = context.getString(item.getStringResource());
-			}
-			DateAsXAxisLabelFormatter labelFormatter = new DateAsXAxisLabelFormatter(context, "HH:mm", "dd.MM");
-			StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView, labelFormatter);
-			staticLabelsFormatter.setVerticalLabels(verlabels);
-			graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-		} else {
-			final String unit = " " + unitsHelper.getStringUnit(baseValue);
-			graphView.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(context, "HH:mm", "dd.MM", unit));
-		}
-	}
-
 	public static void prepareChart(BarLineChartBase chart, final Context context, BaseValue baseValue, ViewGroup layout, Controller controller) {
 
-//		final List<BaseEnumValue.Item> yLabels = ((BaseEnumValue)module.getValue()).getEnumItems();
 		ValueFormatter enumValueFormatter = getValueFormatterInstance(baseValue, context, controller);
 
 		Legend legend = chart.getLegend();
 		legend.setForm(Legend.LegendForm.CIRCLE);
 
-//		chart.setDrawBorders(true);
+		chart.setDrawBorders(true);
+		chart.setBorderColor(context.getResources().getColor(R.color.beeeon_separator));
 		chart.setDescription("");
 		chart.setHighlightEnabled(false);
 
@@ -136,11 +59,11 @@ final public class ChartHelper {
 		chart.getAxisRight().setEnabled(false);
 
 		if (baseValue instanceof BaseEnumValue) {
-			final List<BaseEnumValue.Item> yLabels = ((BaseEnumValue)baseValue).getEnumItems();
+			final List<BaseEnumValue.Item> yLabels = ((BaseEnumValue) baseValue).getEnumItems();
 			if (yLabels.size() > 2) {
 				if (layout.getVisibility() != View.VISIBLE) {
 					int j = 1;
-					for(int i = yLabels.size() - 1; i > -1; i--) {
+					for (int i = yLabels.size() - 1; i > -1; i--) {
 						TextView label = new TextView(context);
 						label.setText(String.format("%d. %s", j++, context.getString(yLabels.get(i).getStringResource())));
 						layout.addView(label);
@@ -157,10 +80,17 @@ final public class ChartHelper {
 		}
 	}
 
+	/**
+	 * Prepare ValueFormatter for bar and line chart
+	 * @param baseValue
+	 * @param context Context
+	 * @param controller Controller instance
+	 * @return specific valueFormatter
+	 */
 	public static ValueFormatter getValueFormatterInstance(final BaseValue baseValue, final Context context, Controller controller) {
 		final UnitsHelper unitsHelper = new UnitsHelper(controller.getUserSettings(), context);
 		if (baseValue instanceof BaseEnumValue) {
-			final List<BaseEnumValue.Item> yLabels = ((BaseEnumValue)baseValue).getEnumItems();
+			final List<BaseEnumValue.Item> yLabels = ((BaseEnumValue) baseValue).getEnumItems();
 			if (yLabels.size() > 2) {
 				return new ValueFormatter() {
 					@Override
