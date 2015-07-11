BeeeOn/android

commit 1d6d5b2b1aa8dd4fa320a9bb8ed56d670092a1dd

Change textSize to textappearance appcompat.

BeeeOn/app/src/main/java/com/rehivetech/beeeon/widget/data/WidgetModuleData.java
@@ -118,7 +118,7 @@ public class WidgetModuleData extends WidgetData {
 					mBuilder.setTextViewText(R.id.widget_last_update, getIsCached() ? String.format("%s " + mContext.getString(R.string.widget_cached), dev.lastUpdateText) : dev.lastUpdateText);
 					break;
 				case R.layout.widget_device_1x1:
-					dev.setValueUnitSize(R.dimen.textsize_caption);
+					dev.setValueUnitSize(R.style.TextAppearance_AppCompat_Caption);
 					break;
 			}
 		}

BeeeOn/app/app.iml
@@ -80,6 +80,7 @@
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/com.melnykov/fab/1.2.0/jars" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/com.sonyericsson.extras.liveware/api/3.0/jars" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/com.sonyericsson.extras.liveware/utils/3.0/jars" />
+      <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/com.vk/androidsdk/1.3.1/jars" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/fr.avianey.com.viewpagerindicator/library/2.4.1/jars" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/net.i2p.android.ext/floatingactionbutton/1.8.0/jars" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/exploded-aar/net.simonvt.numberpicker/number-picker/jars" />
@@ -113,13 +114,12 @@
     <orderEntry type="library" exported="" name="utils-3.0" level="project" />
     <orderEntry type="library" exported="" name="play-services-6.5.87" level="project" />
     <orderEntry type="library" exported="" name="showcaseview-5.1.1" level="project" />
-    <orderEntry type="library" exported="" name="appcompat-v7-22.1.1" level="project" />
     <orderEntry type="library" exported="" name="appcompat-v7-22.2.0" level="project" />
     <orderEntry type="library" exported="" name="joda-time-2.7" level="project" />
     <orderEntry type="library" exported="" name="facebook-android-sdk-4.0.0" level="project" />
     <orderEntry type="library" exported="" name="circleprogress-1.1.0" level="project" />
-    <orderEntry type="library" exported="" name="bolts-android-1.1.4" level="project" />
     <orderEntry type="library" exported="" name="MPAndroidChart-v2.1.0" level="project" />
+    <orderEntry type="library" exported="" name="bolts-android-1.1.4" level="project" />
     <orderEntry type="library" exported="" name="fab-1.2.0" level="project" />
     <orderEntry type="library" exported="" name="api-3.0" level="project" />
     <orderEntry type="library" exported="" name="androidsdk-1.3.1" level="project" />

BeeeOn/app/src/main/res/layout/drawer_listview_profile.xml
@@ -23,8 +23,7 @@
 		android:id="@+id/name"
 		android:layout_width="wrap_content"
 		android:layout_height="wrap_content"
-		android:textSize="14sp"
-		android:fontFamily="sans-serif-medium"
+		android:textAppearance="@style/TextAppearance.AppCompat.Body2"
 		android:layout_above="@+id/email"
 		android:layout_toRightOf="@+id/icon"
 		android:layout_toEndOf="@+id/icon"
@@ -35,9 +34,8 @@
 		android:id="@+id/email"
 		android:layout_width="wrap_content"
 		android:layout_height="wrap_content"
-		android:textSize="14sp"
 		android:textColor="@color/beeeon_text_hint"
-		android:fontFamily="sans-serif-medium"
+		android:textAppearance="@style/TextAppearance.AppCompat.Body2"
 		android:layout_alignBottom="@+id/icon"
 		android:layout_toRightOf="@+id/icon"
 		android:layout_toEndOf="@+id/icon"

BeeeOn/app/src/main/res/layout/fragment_gate_detail.xml
@@ -9,7 +9,7 @@
 		android:layout_width="match_parent"
 		android:layout_height="72dp"
 		android:singleLine="true"
-		android:textSize="@dimen/textsize_headline"
+		android:textAppearance="@style/TextAppearance.AppCompat.Headline"
 		android:textColor="@color/white"
 		android:gravity="center_vertical"
 		android:background="@color/beeeon_primary_cyan"

BeeeOn/app/src/main/res/layout/fragment_gate_edit.xml
@@ -17,7 +17,7 @@
 			android:layout_width="match_parent"
 			android:layout_height="wrap_content"
 			android:text="@string/fragment_gate_edit_gate_name"
-			android:textSize="@dimen/textsize_subhead"
+			android:textAppearance="@style/TextAppearance.AppCompat.Subhead"
 			android:textColor="@color/light_gray"
 			/>
 
@@ -25,7 +25,7 @@
 			android:id="@+id/fragment_gate_edit_text"
 			android:layout_width="match_parent"
 			android:layout_height="wrap_content"
-			android:textSize="@dimen/textsize_display1"
+			android:textAppearance="@style/TextAppearance.AppCompat.Display1"
 			android:textColorLink="@color/white"
 			android:paddingBottom="20dp"
 			android:textColor="@color/white"
@@ -44,7 +44,7 @@
 		android:text="@string/fragment_gate_edit_gate_id_read_only"
 		android:textColor="@color/beeeon_text_hint"
 		android:layout_marginBottom="7dp"
-		android:textSize="@dimen/textsize_caption"
+		android:textAppearance="@style/TextAppearance.AppCompat.Caption"
 		android:layout_marginLeft="72dp"
 		android:layout_marginStart="72dp"
 		/>
@@ -53,7 +53,7 @@
 		android:id="@+id/fragment_gate_edit_gate_id"
 		android:layout_width="wrap_content"
 		android:layout_height="wrap_content"
-		android:textSize="@dimen/textsize_body"
+		android:textAppearance="@style/TextAppearance.AppCompat.Body1"
 		android:textColor="@color/beeeon_text_hint"
 		android:layout_marginLeft="72dp"
 		android:layout_marginStart="72dp"
@@ -70,7 +70,7 @@
 		android:layout_height="wrap_content"
 		android:text="@string/time_zone"
 		android:textColor="@color/beeeon_text_hint"
-		android:textSize="@dimen/textsize_caption"
+		android:textAppearance="@style/TextAppearance.AppCompat.Caption"
 		android:layout_marginLeft="72dp"
 		android:layout_marginStart="72dp"
 		/>

BeeeOn/app/src/main/res/layout/sensor_listview_header.xml
@@ -13,7 +13,6 @@
 		android:layout_marginStart="16dp"
 		android:textColor="@color/beeeon_text_hint"
 		android:layout_gravity="center_vertical"
-		android:textSize="@dimen/textsize_body"
-		android:textStyle="bold"
+		android:textAppearance="@style/TextAppearance.AppCompat.Body2"
 		/>
 </LinearLayout>

BeeeOn/app/src/main/res/layout/sensor_listview_item.xml
@@ -21,7 +21,7 @@
 		android:layout_height="wrap_content"
 		android:singleLine="true"
 		android:textColor="@color/beeeon_text_color"
-		android:textSize="@dimen/textsize_subhead"
+		android:textAppearance="@style/TextAppearance.AppCompat.Subhead"
 		android:layout_toRightOf="@+id/iconofsensor"
 		android:layout_toEndOf="@+id/iconofsensor"
 		android:layout_alignTop="@+id/iconofsensor"
@@ -35,7 +35,7 @@
 		android:layout_height="wrap_content"
 		android:gravity="bottom"
 		android:textColor="@color/beeeon_text_hint"
-		android:textSize="@dimen/textsize_caption"
+		android:textAppearance="@style/TextAppearance.AppCompat.Caption"
 		android:layout_below="@+id/titleofsensor"
 		android:layout_alignLeft="@+id/titleofsensor"
 		android:layout_alignStart="@+id/titleofsensor"
@@ -53,7 +53,7 @@
 		android:scrollHorizontally="true"
 		android:singleLine="true"
 		android:textColor="@color/beeeon_secundary_pink"
-		android:textSize="@dimen/textsize_subhead"
+		android:textAppearance="@style/TextAppearance.AppCompat.Subhead"
 		android:textStyle="bold"
 		android:layout_marginLeft="16dp"
 		android:layout_marginStart="16dp"
@@ -72,7 +72,7 @@
 		android:textColor="@color/beeeon_secundary_pink"
 		android:layout_marginRight="16dp"
 		android:layout_marginEnd="16dp"
-		android:textSize="@dimen/textsize_body"
+		android:textAppearance="@style/TextAppearance.AppCompat.Body1"
 		/>
 
 	<!-- divider -->

BeeeOn/app/src/main/res/layout/simple_list_item_layout.xml
@@ -18,7 +18,7 @@
 		android:layout_width="wrap_content"
 		android:layout_height="wrap_content"
 		android:textColor="@color/beeeon_text_hint"
-		android:textSize="@dimen/textsize_caption"
+		android:textAppearance="@style/TextAppearance.AppCompat.Caption"
 		android:layout_marginTop="20dp"
 		android:layout_marginLeft="72dp"
 		android:layout_marginStart="72dp"
@@ -32,7 +32,7 @@
 		android:id="@+id/simple_list_layout_text"
 		android:layout_width="wrap_content"
 		android:layout_height="wrap_content"
-		android:textSize="@dimen/textsize_subhead"
+		android:textAppearance="@style/TextAppearance.AppCompat.Subhead"
 		android:layout_below="@+id/simple_list_layout_title"
 		android:layout_alignLeft="@+id/simple_list_layout_title"
 		android:layout_alignStart="@+id/simple_list_layout_title"

commit 64db74518be470028942dfab3d8bdb5c1972d326

Fix crash in widgets, introduced in 1d6d5b2b1aa8dd4fa320a9bb8ed56d670092a1dd

BeeeOn/app/src/main/java/com/rehivetech/beeeon/widget/data/WidgetModuleData.java
@@ -118,7 +118,7 @@ public class WidgetModuleData extends WidgetData {
 					mBuilder.setTextViewText(R.id.widget_last_update, getIsCached() ? String.format("%s " + mContext.getString(R.string.widget_cached), dev.lastUpdateText) : dev.lastUpdateText);
 					break;
 				case R.layout.widget_device_1x1:
-					dev.setValueUnitSize(R.style.TextAppearance_AppCompat_Caption);
+					dev.setValueUnitSize(R.dimen.textsize_caption);
 					break;
 			}
 		}