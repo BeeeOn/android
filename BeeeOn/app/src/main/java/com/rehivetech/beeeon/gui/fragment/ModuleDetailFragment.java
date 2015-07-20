1ef2921fed6447f11e44128894cf6717b1fefd64
Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
2015-07-20 16:53:21 +0200

    Rework and fix setting actuator values, enum values now could be set universally

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/fragment/ModuleDetailFragment.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/fragment/ModuleDetailFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
@@ -13,11 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.avast.android.dialogs.fragment.ListDialogFragment;
 import com.avast.android.dialogs.iface.IListDialogListener;
 import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.data.BarData;
@@ -44,7 +45,6 @@ import com.rehivetech.beeeon.household.device.ModuleLog.DataInterval;
 import com.rehivetech.beeeon.household.device.ModuleLog.DataType;
 import com.rehivetech.beeeon.household.device.values.BaseValue;
 import com.rehivetech.beeeon.household.device.values.EnumValue;
-import com.rehivetech.beeeon.household.device.values.TemperatureValue;
 import com.rehivetech.beeeon.household.gate.Gate;
 import com.rehivetech.beeeon.household.location.Location;
 import com.rehivetech.beeeon.threading.CallbackTask.ICallbackTaskListener;
@@ -73,9 +73,7 @@ public class ModuleDetailFragment extends BaseApplicationFragment implements ILi
 	public static final String EXTRA_GATE_ID = "gate_id";
 	public static final String EXTRA_MODULE_ID = "module_id";
 
-
-	private static final int REQUEST_BOILER_TYPE = 7894;
-	private static final int REQUEST_BOILER_MODE = 1236;
+	private static final int REQUEST_SET_ACTUATOR = 7894;
 
 	public static final String ARG_CUR_PAGE = "currentpage";
 	public static final String ARG_SEL_PAGE = "selectedpage";
@@ -124,7 +122,7 @@ public class ModuleDetailFragment extends BaseApplicationFragment implements ILi
 			mActivity = (ModuleDetailActivity) activity;
 		} catch (ClassCastException e) {
 			throw new ClassCastException(activity.toString()
-					+ " must be subclass of SensorDetailActivity");
+					+ " must be subclass of ModuleDetailActivity");
 		}
 	}
 
@@ -222,69 +220,44 @@ public class ModuleDetailFragment extends BaseApplicationFragment implements ILi
 					doActorAction(module);
 				}
 			});
-			final Fragment frg = this;
-			if (module.getValue() instanceof TemperatureValue) {
-				// Set listner for dialog with NumberPicker
-				mValueSet.setOnClickListener(new OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						NumberPickerDialogFragment.show(mActivity, module, frg);
-					}
-				});
 
-			} /* // FIXME: WUT? rework this
-			else if (module.getValue() instanceof BoilerOperationTypeValue) {
-				// Set dialog for set Type of  BOILER
+			if (module.getValue() instanceof EnumValue) {
 				mValueSet.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						String[] tmp = new String[]{
-								getString(R.string.dev_boiler_operation_type_value_off),
-								getString(R.string.dev_boiler_operation_type_value_room),
-								getString(R.string.dev_boiler_operation_type_value_equiterm),
-								getString(R.string.dev_boiler_operation_type_value_stable),
-								getString(R.string.dev_boiler_operation_type_value_tuv),
-						};
+						EnumValue value = (EnumValue) module.getValue();
+						List<EnumValue.Item> items = value.getEnumItems();
+
+						List<String> namesList = new ArrayList<>();
+						for (EnumValue.Item item : items) {
+							namesList.add(getString(item.getStringResource()));
+						}
 
 						ListDialogFragment
 								.createBuilder(mActivity, mActivity.getSupportFragmentManager())
-								.setTitle(getString(R.string.dialog_title_set_bioler_type))
-								.setItems(tmp)
-								.setSelectedItem(((BoilerOperationTypeValue) module.getValue()).getActive().getId())
-								.setRequestCode(REQUEST_BOILER_TYPE)
+								.setTitle(getString(R.string.dialog_actuator_set_value))
+								.setItems(namesList.toArray(new CharSequence[namesList.size()]))
+								.setSelectedItem(value.getActive().getId())
+								.setRequestCode(REQUEST_SET_ACTUATOR)
 								.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE)
-								.setConfirmButtonText(R.string.dialog_set_boiler_setaction)
-								.setCancelButtonText(R.string.notification_cancel)
-								.setTargetFragment(frg, REQUEST_BOILER_TYPE)
+								.setConfirmButtonText(R.string.dialog_actuator_action)
+								.setCancelButtonText(R.string.dialog_actuator_cancel)
+								.setTargetFragment(ModuleDetailFragment.this, REQUEST_SET_ACTUATOR)
 								.show();
 					}
 				});
-			} else if (module.getValue() instanceof BoilerOperationModeValue) {
-				// Set dialog for set Mode of Boiler
+			} else {
+				// FIXME: support all kinds of value types and units etc.
+				// BaseValue value = mModule.getValue();
+
 				mValueSet.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						String[] tmp = new String[]{
-								getString(R.string.dev_boiler_operation_mode_value_automatic),
-								getString(R.string.dev_boiler_operation_mode_value_manual),
-								getString(R.string.dev_boiler_operation_mode_value_vacation)
-						};
-
-						ListDialogFragment
-								.createBuilder(mActivity, mActivity.getSupportFragmentManager())
-								.setTitle(getString(R.string.dialog_title_set_bioler_mode))
-								.setItems(tmp)
-								.setSelectedItem(((BoilerOperationModeValue) module.getValue()).getActive().getId())
-								.setRequestCode(REQUEST_BOILER_MODE)
-								.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE)
-								.setConfirmButtonText(R.string.dialog_set_boiler_setaction)
-								.setCancelButtonText(R.string.notification_cancel)
-								.setTargetFragment(frg, REQUEST_BOILER_MODE)
-								.show();
+						Log.d(TAG, "SET TEMPERATURE");
+						NumberPickerDialogFragment.show(mActivity, module, ModuleDetailFragment.this);
 					}
 				});
-			}*/
-
+			}
 		}
 
 		// Set name of location
@@ -303,11 +276,15 @@ public class ModuleDetailFragment extends BaseApplicationFragment implements ILi
 		// Set value of sensor
 		if (mUnitsHelper != null) {
 			mValue.setText(mUnitsHelper.getStringValueUnit(module.getValue()));
-			BaseValue val = module.getValue();
-			// FIXME: rework this
-			/*if (val instanceof OnOffValue) {
-				mValueSwitch.setChecked(((BooleanValue) val).isActive());
-			}*/
+			// FIXME: rework this better
+			if (module.getValue() instanceof EnumValue) {
+				EnumValue value = (EnumValue) module.getValue();
+				List<EnumValue.Item> items = value.getEnumItems();
+				if (items.size() == 2) {
+					int index = items.indexOf(value.getActive());
+					mValueSwitch.setChecked(index == 1);
+				}
+			}
 		}
 
 		// Set icon of sensor
@@ -354,13 +331,15 @@ public class ModuleDetailFragment extends BaseApplicationFragment implements ILi
 		if (module.isActuator() && controller.isUserAllowed(gate.getRole())) {
 			BaseValue value = module.getValue();
 
-			// For actor values of type on/off, open/closed we show switch button
-			// FIXME: rework this
-			/*if (value instanceof OnOffValue || value instanceof OpenClosedValue) {
+			// FIXME: rework this better
+			// Show specific control view
+			if (value instanceof EnumValue && ((EnumValue) value).getEnumItems().size() == 2) {
+				// For enum actuators with 2 values show a switch Button
 				mValueSwitch.setVisibility(View.VISIBLE);
-			} else if (value instanceof TemperatureValue || value instanceof BoilerOperationModeValue || value instanceof BoilerOperationTypeValue) {
+			} else {
+				// For other actuators show value set button
 				mValueSet.setVisibility(View.VISIBLE);
-			}*/
+			}
 		}
 
 		if (controller.isUserAllowed(gate.getRole())) {
@@ -586,7 +565,7 @@ public class ModuleDetailFragment extends BaseApplicationFragment implements ILi
 
 	@Override
 	public void onListItemSelected(CharSequence value, int number, int requestCode) {
-		if (requestCode == REQUEST_BOILER_MODE || requestCode == REQUEST_BOILER_TYPE) {
+		if (requestCode == REQUEST_SET_ACTUATOR || requestCode == REQUEST_SET_ACTUATOR) {
 			Module module = Controller.getInstance(mActivity).getDevicesModel().getModule(mGateId, mModuleId);
 			if (module == null) {
 				Log.e(TAG, "Can't load module for changing its value");
