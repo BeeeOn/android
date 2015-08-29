73bed8375411502b85a8257748437fda326b47d0
xkozak15 <dkozak94@gmail.com>
2015-08-29 14:24:48 +0200

    enable to change language in preferences
    started using stringarrays in values instead of creating them in code

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/activity/SettingsActivity.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/activity/SettingsActivity.java
@@ -1,14 +1,22 @@
 package com.rehivetech.beeeon.gui.activity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.rehivetech.beeeon.R;
 import com.rehivetech.beeeon.gui.fragment.MainSettingsFragment;
 
+import java.util.Locale;
+
 /**
  * Created by david on 26.8.15.
  */
-public class SettingsActivity extends BaseApplicationActivity {
+public class SettingsActivity extends BaseApplicationActivity implements MainSettingsFragment.onPreferenceChangedListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +27,29 @@ public class SettingsActivity extends BaseApplicationActivity {
 			mActionBar.setDisplayHomeAsUpEnabled(true);
 		}
 
-		getSupportFragmentManager().beginTransaction().replace(R.id.settings_activity_fragment_holder,new MainSettingsFragment()).commit();
+		getSupportFragmentManager().beginTransaction().replace(R.id.settings_activity_fragment_holder, new MainSettingsFragment()).commit();
 	}
+
+	public void setLocale(String lang) {
+		Locale locale = new Locale(lang);
+		Resources res = getResources();
+		DisplayMetrics dm = res.getDisplayMetrics();
+		Configuration conf = res.getConfiguration();
+		conf.locale = locale;
+		res.updateConfiguration(conf, dm);
+		Intent refresh = new Intent(this, SettingsActivity.class);
+		startActivity(refresh);
+		finish();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				finish();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/fragment/MainSettingsFragment.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/fragment/MainSettingsFragment.java
@@ -1,17 +1,60 @@
 package com.rehivetech.beeeon.gui.fragment;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceManager;
 import android.support.v4.preference.PreferenceFragmentCompat;
 
 import com.rehivetech.beeeon.R;
+import com.rehivetech.beeeon.controller.Controller;
 
 /**
  * Created by david on 26.8.15.
  */
-public class MainSettingsFragment extends PreferenceFragmentCompat {
+public class MainSettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+	private onPreferenceChangedListener mOnPreferenceChangedListener;
+	private ListPreference mLanguagePref;
+	private ListPreference mTimeZonePref;
+
 	@Override
 	public void onCreate(Bundle paramBundle) {
 		super.onCreate(paramBundle);
 		addPreferencesFromResource(R.xml.activity_settings_main_preferences);
+
+		SharedPreferences prefs = Controller.getInstance(getActivity()).getUserSettings();
+		if (prefs == null) {
+			getActivity().finish(); // TODO: use better way to exit
+			return;
+		}
+		PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
+
+		mLanguagePref = (ListPreference) findPreference("pref_language");
+		mTimeZonePref = (ListPreference) findPreference("timezone_pref_array");
+
+
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			mOnPreferenceChangedListener = (onPreferenceChangedListener) getActivity();
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString() + " must implement onViewSelected");
+		}
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals("pref_language")) {
+			mOnPreferenceChangedListener.setLocale(sharedPreferences.getString(key, null));
+		}
+	}
+
+	public interface onPreferenceChangedListener {
+		void setLocale(String lang);
 	}
 }
