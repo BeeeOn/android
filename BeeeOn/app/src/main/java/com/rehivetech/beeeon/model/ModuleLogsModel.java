07501cc92eb3abb1cece994b4e6b6ce0d01388f3
Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
2015-12-08 23:26:03 +0100

    Temp comment out module log data cache

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/model/ModuleLogsModel.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/model/ModuleLogsModel.java
@@ -4,8 +4,6 @@ import android.util.Log;
 
 import com.rehivetech.beeeon.exception.AppException;
 import com.rehivetech.beeeon.household.device.ModuleLog;
-import com.rehivetech.beeeon.household.device.ModuleLog.DataInterval;
-import com.rehivetech.beeeon.household.device.ModuleLog.DataType;
 import com.rehivetech.beeeon.network.INetwork;
 
 import org.joda.time.Interval;
@@ -17,7 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.SortedMap;
 
 public class ModuleLogsModel extends BaseModel {
 
@@ -35,13 +32,14 @@ public class ModuleLogsModel extends BaseModel {
 		ModuleLog data = mModulesLogs.get(moduleId);
 		if (data == null) {
 			// Just save new item
-			mModulesLogs.put(moduleId, log);
-		} else {
-			// We need to append these values to existing log
-			for (Entry<Long, Float> entry : log.getValues().entrySet()) {
-				data.addValue(entry.getKey(), entry.getValue());
-			}
 		}
+			mModulesLogs.put(moduleId, log);
+//		else {
+			// We need to append these values to existing log
+//			for (Entry<Long, Float> entry : log.getValues().entrySet()) {
+//				data.addValue(entry.getKey(), entry.getValue());
+//			}
+//		}
 	}
 
 	private List<Interval> getMissingIntervals(ModuleLog.DataPair pair) {
@@ -51,63 +49,66 @@ public class ModuleLogsModel extends BaseModel {
 
 		Log.d(TAG, String.format("We want interval: %s -> %s", fmt.print(interval.getStart()), fmt.print(interval.getEnd())));
 
-		if (!mModulesLogs.containsKey(moduleId)) {
-			// No log for this module, download whole interval
-			Log.d(TAG, String.format("No cached log for module %s", moduleId));
-			downloadIntervals.add(interval);
-		} else {
-			// We have this ModuleLog with (not necessarily all) values
-			ModuleLog data = mModulesLogs.get(moduleId);
-
-			// Values are returned as sorted
-			SortedMap<Long, Float> rows = data.getValues();
-			if (rows.isEmpty()) {
-				// No values in this log, download whole interval
-				Log.d(TAG, String.format("We have log, but with no values for module %s", moduleId));
-				downloadIntervals.add(interval);
-			} else {
-				// Use pair gap to make sure we won't make useless request when there won't be no new value anyway
-				int gap = pair.gap.getSeconds() * 1000;
-
-				// Determine missing interval
-				long first = rows.firstKey();
-				long last = rows.lastKey();
-
-				Log.d(TAG, String.format("We have cached: %s -> %s for module %s", fmt.print(first), fmt.print(last), moduleId));
-
-				Log.d(TAG, String.format("We have log and there are some values for module %s", moduleId));
-				Log.v(TAG, String.format("Gap: %d ms", gap));
-
-				if (interval.isBefore(first) || interval.isAfter(last)) {
-					// Outside of values in this log, download whole interval
-					Log.d(TAG, String.format("Wanted interval is before or after cached interval for module %s", moduleId));
-					downloadIntervals.add(interval);
-					// TODO: remember this new hole?
-				} else {
-					if (interval.contains(first)) {
-						Log.d(TAG, String.format("Wanted interval contains FIRST of cached interval for module %s", moduleId));
-						// <start of interval, start of saved data> 
-						Interval cutInterval = new Interval(interval.getStartMillis(), first);
-						Log.v(TAG, String.format("Cut (FIRST) interval: %s -> %s for module %s", fmt.print(cutInterval.getStart()), fmt.print(cutInterval.getEnd()), moduleId));
-						Log.v(TAG, String.format("Is (FIRST) interval duration: %d > gap: %d ?", cutInterval.toDurationMillis(), gap));
-						if (cutInterval.toDurationMillis() > gap)
-							downloadIntervals.add(cutInterval);
-					}
-
-					if (interval.contains(last)) {
-						Log.d(TAG, String.format("Wanted interval contains LAST of cached interval for module %s", moduleId));
-						// <end of saved data, end of interval>
-						Interval cutInterval = new Interval(last, interval.getEndMillis());
-						Log.v(TAG, String.format("Cut (LAST) interval: %s -> %s for module %s", fmt.print(cutInterval.getStart()), fmt.print(cutInterval.getEnd()), moduleId));
-						Log.v(TAG, String.format("Is (LAST) interval duration: %d > gap: %d ?", cutInterval.toDurationMillis(), gap));
-						if (cutInterval.toDurationMillis() > gap)
-							downloadIntervals.add(cutInterval);
-					}
-				}
-			}
-		}
-
+		downloadIntervals.add(interval);
 		return downloadIntervals;
+
+//		if (!mModulesLogs.containsKey(moduleId)) {
+//			// No log for this module, download whole interval
+//			Log.d(TAG, String.format("No cached log for module %s", moduleId));
+//			downloadIntervals.add(interval);
+//		} else {
+//			// We have this ModuleLog with (not necessarily all) values
+//			ModuleLog data = mModulesLogs.get(moduleId);
+//
+//			// Values are returned as sorted
+//			SortedMap<Long, Float> rows = data.getValues();
+//			if (rows.isEmpty()) {
+//				// No values in this log, download whole interval
+//				Log.d(TAG, String.format("We have log, but with no values for module %s", moduleId));
+//				downloadIntervals.add(interval);
+//			} else {
+//				// Use pair gap to make sure we won't make useless request when there won't be no new value anyway
+//				int gap = pair.gap.getSeconds() * 1000;
+//
+//				// Determine missing interval
+//				long first = rows.firstKey();
+//				long last = rows.lastKey();
+//
+//				Log.d(TAG, String.format("We have cached: %s -> %s for module %s", fmt.print(first), fmt.print(last), moduleId));
+//
+//				Log.d(TAG, String.format("We have log and there are some values for module %s", moduleId));
+//				Log.v(TAG, String.format("Gap: %d ms", gap));
+//
+//				if (interval.isBefore(first) || interval.isAfter(last)) {
+//					// Outside of values in this log, download whole interval
+//					Log.d(TAG, String.format("Wanted interval is before or after cached interval for module %s", moduleId));
+//					downloadIntervals.add(interval);
+//					// TODO: remember this new hole?
+//				} else {
+//					if (interval.contains(first)) {
+//						Log.d(TAG, String.format("Wanted interval contains FIRST of cached interval for module %s", moduleId));
+//						// <start of interval, start of saved data>
+//						Interval cutInterval = new Interval(interval.getStartMillis(), first);
+//						Log.v(TAG, String.format("Cut (FIRST) interval: %s -> %s for module %s", fmt.print(cutInterval.getStart()), fmt.print(cutInterval.getEnd()), moduleId));
+//						Log.v(TAG, String.format("Is (FIRST) interval duration: %d > gap: %d ?", cutInterval.toDurationMillis(), gap));
+//						if (cutInterval.toDurationMillis() > gap)
+//							downloadIntervals.add(cutInterval);
+//					}
+//
+//					if (interval.contains(last)) {
+//						Log.d(TAG, String.format("Wanted interval contains LAST of cached interval for module %s", moduleId));
+//						// <end of saved data, end of interval>
+//						Interval cutInterval = new Interval(last, interval.getEndMillis());
+//						Log.v(TAG, String.format("Cut (LAST) interval: %s -> %s for module %s", fmt.print(cutInterval.getStart()), fmt.print(cutInterval.getEnd()), moduleId));
+//						Log.v(TAG, String.format("Is (LAST) interval duration: %d > gap: %d ?", cutInterval.toDurationMillis(), gap));
+//						if (cutInterval.toDurationMillis() > gap)
+//							downloadIntervals.add(cutInterval);
+//					}
+//				}
+//			}
+//		}
+//
+//		return downloadIntervals;
 	}
 
 	/**
@@ -117,7 +118,7 @@ public class ModuleLogsModel extends BaseModel {
 	 * @return
 	 */
 	public ModuleLog getModuleLog(ModuleLog.DataPair pair) {
-		ModuleLog log = new ModuleLog(DataType.AVERAGE, DataInterval.RAW);
+			ModuleLog log = new ModuleLog(pair.type, pair.gap);
 
 		String moduleId = pair.module.getModuleId().absoluteId;
 		if (mModulesLogs.containsKey(moduleId)) {
