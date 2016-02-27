9f3359d510e83efc6d6ef2d34b70ccff720991d7
Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
2016-02-27 18:44:31 +0100

    Add to Device model method to get modules by group name

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/household/device/Device.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/household/device/Device.java
@@ -415,6 +415,24 @@ public final class Device implements IIdentifier {
 	}
 
 	/**
+	 * Get all modules by group name
+	 * @param context context to get string resource
+	 * @param groupName group name
+	 * @return list of modules
+	 */
+	public List<Module> getModulesByGroupName(Context context, String groupName) {
+		List<Module> modules = getVisibleModules();
+		Iterator<Module> iterator = modules.iterator();
+		while (iterator.hasNext()) {
+			Module module = iterator.next();
+			if (!module.getGroupName(context).equals(groupName)) {
+				iterator.remove();
+			}
+		}
+		return modules;
+	}
+
+	/**
 	 * Data holder used for saving device data to server.
 	 */
 	public static class DataPair {
