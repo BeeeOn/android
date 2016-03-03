5317603224a970451bdc3cdfb0cb180e68221ee8
xkozak15 <dkozak94@gmail.com>
2016-03-03 13:07:22 +0100

    finish basic implementation

--- tests/monkey-new/monkey1.sh
+++ tests/monkey-new/monkey1.sh
@@ -9,10 +9,20 @@ echo $device
 echo $PACKAGE_NAME
 
 adb -s ${device} shell monkey -p ${PACKAGE_NAME} -c android.intent.category.LAUNCHER 1;
+sleep 3
 
-PID=$(adb shell ps | grep ${APP_NAME} | tr -s ' ' | cut -d ' ' -f 2)
-echo "PID of the beeon app process is : ${PID}, killing..."
+
+for i in $(seq 1 ${ITER}) ; do
+	adb shell monkey -p ${PACKAGE_NAME} ${EVENT}
+	sleep 1
+done 
+
+
+#PID=$(adb shell ps | grep ${APP_NAME} | tr -s ' ' | cut -d ' ' -f 2)
+#echo "PID of the beeon app process is : ${PID}, killing..."
 #adb -s ${device} shell kill ${PID}
+
+
 adb shell am force-stop ${PACKAGE_NAME}
 
 echo "Test finished"
