7defa6ab833c9e376e4ae16282db8c8d8a834107
Tomáš Mlynarič <mlynarict@gmail.com>
2016-06-06 15:31:04 +0200

    Picking files for certificate [TMP]

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/dialog/ServerDetailDialog.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/dialog/ServerDetailDialog.java
@@ -2,16 +2,24 @@ package com.rehivetech.beeeon.gui.dialog;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.rehivetech.beeeon.R;
 import com.rehivetech.beeeon.model.entity.Server;
 import com.rehivetech.beeeon.util.Utils;
 
+import butterknife.ButterKnife;
 import io.realm.Realm;
 
 /**
@@ -20,6 +28,10 @@ import io.realm.Realm;
  */
 public class ServerDetailDialog extends BaseBeeeOnDialog {
 	private static final String ARG_SERVER_ID = "server_id";
+	private static final int REQUEST_CERTIFICATE = 1;
+	private static final int PERMISSION_STORAGE = 10;
+	private static final String TAG = ServerDetailDialog.class.getSimpleName();
+
 	private long mServerId;
 	private Realm mRealm;
 	private TextInputLayout mNameView;
@@ -131,10 +143,89 @@ public class ServerDetailDialog extends BaseBeeeOnDialog {
 			setDeleteButton(builder, getNeutralButtonText());
 		}
 
+		Button btn = ButterKnife.findById(mRootView, R.id.server_certificate_button);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				pickFileWithPermissionCheck();
+			}
+		});
+
 		return builder;
 	}
 
-	public static class ServerDetailDialogBuilder extends BaseBeeeOnDialogBuilder {
+	private void pickFileWithPermissionCheck() {
+		Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+		getIntent.setType("application/x-x509-ca-cert");
+		getIntent.addCategory(Intent.CATEGORY_OPENABLE);
+		Intent chooserIntent = Intent.createChooser(getIntent, "Select certificate");
+
+//		Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//		pickIntent.setType("image/*");
+//		chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+		startActivityForResult(chooserIntent, REQUEST_CERTIFICATE);
+	}
+
+//	private void checkDataPermission() {
+//		if (ContextCompat.checkSelfPermission(getActivity(),
+//				Manifest.permission.READ_EXTERNAL_STORAGE)
+//				!= PackageManager.PERMISSION_GRANTED) {
+//			// Should we show an explanation?
+//			if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//					Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//
+//				// Show an explanation to the user *asynchronously* -- don't block
+//				// this thread waiting for the user's response! After the user
+//				// sees the explanation, try again to request the permission.
+//				ActivityCompat.requestPermissions(this,
+//						new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+//						PERMISSION_STORAGE);
+//
+//			} else {
+//				// No explanation needed, we can request the permission.
+//				if (DeviceUtils.getPermissionStorage()) {
+//					startActivity(Utils.getAppSettingsIntent());
+//				} else {
+//					DeviceUtils.setPermissionStorage();
+//					ActivityCompat.requestPermissions(this,
+//							new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+//							PERMISSION_STORAGE);
+//				}
+//			}
+//		} else {
+//			addImageAttachment();
+//		}
+//	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode != REQUEST_CERTIFICATE) {
+			Log.d(TAG, "Not handled activity result");
+			return;
+		}
+
+		if (data == null) {
+			Log.e(TAG, "No data from activity");
+			return;
+		}
+
+		Uri uriData = data.getData();
+		Toast.makeText(getActivity(), uriData.toString(), Toast.LENGTH_LONG).show();
+
+		String imagePath;
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+//			imagePath = Utils.getImagePathOldApi(uriData);
+		} else {
+//			imagePath = Utils.getImagePathNewApi(uriData);
+		}
+//		String name = getFileNameFromPath();
+//		if (name != null) {
+//			attachmentsValue.setText(name);
+	}
+
+	public static class ServerDetailDialogBuilder extends BaseBeeeOnDialog.BaseBeeeOnDialogBuilder {
 		private long mServerId;
 
 		public ServerDetailDialogBuilder(Context context, FragmentManager fragmentManager) {
