eb17f92ef85e0bd3fd79832c2832e26d3a936dc3
dkozak <dkozak94@gmail.com>
2016-06-15 22:31:16 +0200

    add killing of all emulators at the end of the main skript
    just in case the adb emu kill inside stress test is not enough

--- tests/monkey-new/JENKINS_start_test_vol_1.sh
+++ tests/monkey-new/JENKINS_start_test_vol_1.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-./startEmulator.sh
+./start_emulator.sh
 
-./stressTest.sh
+./stress_test.sh
 
-echo "Testing was finished"
+echo "Testing was finished, now I kill all emulators left(if there are any)"
+
+for emu in $(pgrep emulator64-x86)
+do
+        echo "Killing proc ${emu}"
+        kill -9 ${emu}
+done
+
+echo "All emulators are dead, exiting..."
