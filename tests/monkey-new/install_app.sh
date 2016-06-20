85d81e80c7243b153d0019925277978eb680fb94
dkozak <dkozak94@gmail.com>
2016-06-20 12:44:08 +0200

    make the install_app script always use the "artifact" apk

--- tests/monkey-new/install_app.sh
+++ tests/monkey-new/install_app.sh
@@ -1,18 +1,16 @@
 #/bin/bash
 #Script reinstalls the app
 
-# TODO find a better way to use these variables than copy-past from stressTest
-DIR=$(pwd) 
-APK="../../artifacts/app-debug.apk"
 export PACKAGE_NAME="com.rehivetech.beeeon.debug"
 
 #check for parameter --download-new - it is used for getting the .apk file if neccessary
 if [ ! -z ${1} ] ; then
 	if [ ${1} == "--download-new" ] ; then 
 		#downloads the .apk file if neccessary
-		if [ ! -f ${DIR}/${APK}  ] ; then
+		if [ ! -f ${APK_LOCATION}  ] ; then
 			echo "Apk was not found, started downloading of new one" >&2
 			wget ${APK_NET_PATH} --no-check-certificate	
+			mv app-debug.apk ${APK_LOCATION}
 		else
 			echo "Apk file was found, no need to download new one"
 		fi
@@ -23,4 +21,4 @@ fi
 echo "Uninstalling old version of the app"
 adb -s ${devices} uninstall $PACKAGE_NAME
 echo "Installing the new version"
-adb -s ${devices} install ${DIR}/${APK}
+adb -s ${devices} install ${APK_LOCATION}
