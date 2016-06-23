24c235f15f79f25ad6fe706d5206d1f9d6d0ac9b
travis-ci <travis@travis-ci.org>
2016-06-23 10:00:40 +0200

    add special method to device handler, that touches the screen on coordinates from user input

--- tests/monkey-new/test-cases/deviceHandler.py
+++ tests/monkey-new/test-cases/deviceHandler.py
@@ -1,6 +1,11 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 from com.android.monkeyrunner.easy import EasyMonkeyDevice, By 
 
+import sys
+from sys import stdin
+import os
+import subprocess
+
 class DeviceHandler():
 	package = 'com.rehivetech.beeeon.debug'
 	main_activity = 'com.rehivetech.beeeon.gui.activity.LoginActivity'
@@ -14,7 +19,8 @@ class DeviceHandler():
 		self.device.press('KEYCODE_HOME',MonkeyDevice.DOWN_AND_UP)
 		kill_command = 'am force-stop %s' % DeviceHandler.package
 		self.device.shell(kill_command)
-		self.insert_sleep(time=1)
+		self.insert_sleep(time=3)
+		self.err_snapshot_count = 0
 
 		apk_path = self.device.shell('pm path ' + DeviceHandler.package)
 		if apk_path.startswith('package:'):
@@ -38,16 +44,39 @@ class DeviceHandler():
 			MonkeyRunner.sleep(time)
 
 	def press_button(self,id):
-		self.easyDevice.touch(By.id('id/' + id),MonkeyDevice.DOWN_AND_UP)
-		self.insert_sleep()
+		try:
+			self.easyDevice.touch(By.id('id/' + id),MonkeyDevice.DOWN_AND_UP)
+			self.insert_sleep()
+		except Exception, e:
+			print('Button was not found,saving snapshot of the screen')
+			self.save_snapshot('button_not_found' + str(self.err_snapshot_count),'png')
+			self.err_snapshot_count += 1
+			raise e
 
 	def touch_the_screen(self,x,y):
 		self.device.touch(x,y,MonkeyDevice.DOWN_AND_UP)
+		#subprocess.Popen(['adb','shell' ,'input' ,'tap' ,str(x), str(y)])
+		#self.device.shell('input tap ' + str(x) + ' ' + str(y))
 		self.insert_sleep()
+
+	def touch_the_screen_on_given_coordinates(self):
+		while True:
+			try:
+				print("Give me coordinates x y : ")
+				a = stdin.readline()
+				a = a.split(' ')
+				x = int(a[0])
+				y = int(a[1])
+				print("Coordinates: " + a[0] + " " + a[1])
+				self.touch_the_screen(x,y)
+				break
+			except:
+				print("Input was not valid, try again...")
+
 
 	def type_text(self,text):
 		self.device.type(text)
-		self.insert_sleep(time=3)
+		self.insert_sleep()
 
 	def take_snapshot(self):
 		return self.device.takeSnapshot()
@@ -60,9 +89,12 @@ class DeviceHandler():
 		snapshot = self.take_snapshot()
 		other = MonkeyRunner.loadImageFromFile(file_name)
 		if not snapshot.sameAs(other,0.9):
-			self.error("Snaphots are not equal")
+			snapshot.writeToFile('add_gateway_bad_result.png','png')
+			self.error("Snaphots are not equal") 
+			sys.exit(1)
 		else:
 			print("Snapshots are equal")
 
 	def error(self,msg):
 		print(msg)
+		sys.exit(1)
