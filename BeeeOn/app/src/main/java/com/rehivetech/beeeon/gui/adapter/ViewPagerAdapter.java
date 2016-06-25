84da0c33edd28fe4439d27554518c6b9d4928ddd
Martin Matejčík <xmatej46@stud.fit.vutbr.cz>
2016-06-25 10:47:02 +0200

    Fixed dashboard viewpager fragments refresh

--- BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/adapter/ViewPagerAdapter.java
+++ BeeeOn/app/src/main/java/com/rehivetech/beeeon/gui/adapter/ViewPagerAdapter.java
@@ -1,14 +1,8 @@
 package com.rehivetech.beeeon.gui.adapter;
 
-import android.content.Context;
-import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.view.ViewGroup;
-
-import com.rehivetech.beeeon.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +16,9 @@ import java.util.List;
 public class ViewPagerAdapter extends android.support.v4.app.FragmentStatePagerAdapter {
 	private List<Fragment> mFragments = new ArrayList<>();
 	private List<String> mFragmentTitles = new ArrayList<>();
-	private FragmentManager mFragmentManager;
 
 	public ViewPagerAdapter(FragmentManager fm) {
 		super(fm);
-		mFragmentManager = fm;
 	}
 
 	@Override
@@ -45,12 +37,7 @@ public class ViewPagerAdapter extends android.support.v4.app.FragmentStatePagerA
 	}
 
 	public Fragment getActiveFragment(ViewPager container) {
-		String name = makeFragmentName(container.getId(), container.getCurrentItem());
-		return mFragmentManager.findFragmentByTag(name);
-	}
-
-	private static String makeFragmentName(int viewId, int index) {
-		return "android:switcher:" + viewId + ":" + index;
+		return mFragments.get(container.getCurrentItem());
 	}
 
 	public void addFragment(Fragment fragment, String title) {
